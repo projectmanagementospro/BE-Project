3b6d92ca0a1d2fac2f9f033dd80ede3c4f7b248a
Hael12122002 <hendralijaya@gmail.com>
2022-10-22 13:11:09 +0700

    asd

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"hendralijaya/user-management-project/app/config"
+	"hendralijaya/user-management-project/app/routes"
+	"hendralijaya/user-management-project/helper"
+	"hendralijaya/user-management-project/middleware"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	cors "github.com/rs/cors/wrapper/gin"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB = config.NewDB()
+)
+
+func main() {
+	defer config.CloseDB(db)
+	err := godotenv.Load()
+	helper.PanicIfError(err)
+	router := NewRouter()
+	log.Fatal(router.Run(":" + os.Getenv("GO_PORT")))
+}
+
+func NewRouter() *gin.Engine {
+	err := godotenv.Load()
+	helper.PanicIfError(err)
+	/**
+	@description Setup Database Connection
+	*/
+
+	/**
+	@description Init Router
+	*/
+	router := gin.Default()
+	/**
+	@description Setup Mode Application
+	*/
+	if os.Getenv("GO_ENV") != "production" && os.Getenv("GO_ENV") != "test" {
+		gin.SetMode(gin.DebugMode)
+	} else if os.Getenv("GO_ENV") == "test" {
+		gin.SetMode(gin.TestMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	/**
+	@description Setup Middleware
+	*/
+
+	/**
+	@description Init All Route
+	*/
+	// routes.NewAuthenticationRoutes(db, router)
+	// routes.NewUserRoutes(db, router)
+	// // routes.NewRoleRoutes(db, router)
+	// router.Use(middleware.ErrorHandler())
+	// router.Use(cors.Default())
+
+	return router
+}
